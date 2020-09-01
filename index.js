35ae62facde39484622f488ca58666274faaa9c7
pbedrosian <peter_bedrosian@yahoo.com>
2020-09-01 14:17:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 10) + 1
+
+const mod = 16 % 6
+
+const max = Math.max(5, 10, 15, 20)
